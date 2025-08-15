5917f19f764570f1cafed1c7cb8758fccc8daba4
Sala3ni <salonimishramishra9527@gmail.com>
2025-08-15 21:40:55 +0530

    changes

--- server.js
+++ server.js
@@ -26,6 +26,11 @@ if(process.env.NODE_ENV !== "production"){
 app.use(express.json({limit:"5mb"}));
 app.use(cookieParser());
 
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
+
+
 app.use("/api/v1/auth",authRoutes);
 app.use("/api/v1/users",userRoutes);
 app.use("/api/v1/posts",postRoutes);
